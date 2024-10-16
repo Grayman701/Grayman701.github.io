90a005ad8a62bd20195d09916f64f4201755b578
Grayman701 <88445833+Grayman701@users.noreply.github.com>
2024-10-15 19:15:53 -0500

    Delete hello directory

--- hello/code.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementById("b1").addEventListener("click", onclick)
-// ** trying to figure this part out **
-// function onclick () {
-//     document.getElementById("heading").value 
-//     document.getElementById("heading").innerHTML = "Hi, I hacked you."
-//     document.getElementById("heading").innerHTML = document.getElementById("data").value
-// }
